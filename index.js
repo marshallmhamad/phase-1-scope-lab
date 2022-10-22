3fb2841e721b624a63454cd5ffc3333a3faa889e
marshall <mhamad.marshall@gmail.com>
2022-10-22 11:33:24 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var bestCustomer;
+function setBestCustomer(){
+   bestCustomer = 'not bob';
+   return bestCustomer;
+}
+
+var customerName = "bob"
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName
+}
+
+var bestCustomer;
+function overwriteBestCustomer() {
+bestCustomer = "maybe bob"
+    return bestCustomer
+}
+
+const leastFavoriteCustomer = "perhaps bob"
+function changeLeastFavoriteCustomer() {
+   leastFavoriteCustomer = "perhaps bob"
+    return leastFavoriteCustomer
+}
